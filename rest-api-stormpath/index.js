92ad9645d36045ebec3c3b96dedbe70f0b52808e
kserin <kserin@gmail.com>
2016-05-16 09:05:48 +0300

    add auth

--- rest-api-stormpath/index.js
+++ rest-api-stormpath/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
- 
+
+var stormpath = require('express-stormpath');
+
 var app = express();
- 
-app.get('/notes', function(req, res) {
-  res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+
+app.use(stormpath.init(app, {
+  expand: {
+    customData: true,
+  },
+  web: {
+    produces: ['application/json']
+  }
+}));
+
+app.get('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
+  res.json({notes: req.user.customData.notes || "This is your notebook. Edit this to start saving your notes!"})
 });
- 
+
 app.listen(3000);
